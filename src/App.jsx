2ff487423a8538b864383f4ac033d8b38a8810ec
Gustavo Costa <gustavo.lcostaa@gmail.com>
2023-03-31 12:42:54 -0700

    added browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.scss";
 import InternalExternal from "./components/InternalExternal/InternalExternal";
 import Home from "./pages/Home/Home";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Creation from "./components/Creation/Creation";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<DataSet />} />
+          <Route path="/internalorexternal" element={<InternalExternal />} />
+          <Route path="/selectlocation" element={<Creation />} />
+          <Route path="/selectcategory" element={<Category />} />
+          <Route path="/data" element={<DataVisualization />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,19 @@ import MainContent from "../../components/MainContent/MainContent";
 import Navbar from "../../components/Navbar/Navbar";
 import Creation from "../../components/Creation/Creation";
 import "./Home.scss";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="grid">
-      <Navbar />
-      <Header />
-      <MainContent />
-    </div>
+    <>
+      <Link to="/create" style={{ "text-decoration": "none" }}>
+        <div className="grid">
+          <Navbar />
+          <Header />
+          <MainContent />
+        </div>
+      </Link>
+    </>
   );
 }
 
