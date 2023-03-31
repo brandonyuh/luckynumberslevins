e223c955e830c565f23375f6506fa4ea1101dadb
Felipe Gonzalez <fgonzalezcare@gmail.com>
2023-03-31 12:35:10 -0700

    create visualization page with its cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import "./App.scss";
-import InternalExternal from './components/InternalExternal/InternalExternal';
-import Home from "./pages/Home/Home";
+import './App.scss';
+// import InternalExternal from './components/InternalExternal/InternalExternal';
+// import Home from './pages/Home/Home';
+import Visualization from './pages/Visualization/Visualization';
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+   return (
+      <div className='App'>
+         {/* <Home /> */}
+         <Visualization />
+      </div>
+   );
 }
 
 export default App;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,17 @@
-import Header from "../../components/Header/Header";
-import MainContent from "../../components/MainContent/MainContent";
-import Navbar from "../../components/Navbar/Navbar";
-import Creation from "../../components/Creation/Creation";
-import "./Home.scss";
+import Header from '../../components/Header/Header';
+import MainContent from '../../components/MainContent/MainContent';
+import Navbar from '../../components/Navbar/Navbar';
+// import Creation from "../../components/Creation/Creation";
+import './Home.scss';
 
 function Home() {
-  return (
-    <div className="grid">
-      <Navbar />
-      <Header />
-      <MainContent />
-    </div>
-  );
+   return (
+      <div className='grid'>
+         <Navbar />
+         <Header />
+         <MainContent />
+      </div>
+   );
 }
 
 export default Home;
